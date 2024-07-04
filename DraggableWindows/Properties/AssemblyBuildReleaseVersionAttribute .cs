7fd37632adda51ceb4a09356074acadf15512f3d
Mrkingfisher <hellnahugetmymail@gmail.com>
2024-07-04 17:37:44 +0200

    gets AssemblyBuildReleaseVersion parameter/field from AssemblyInfo.cs ,

---
+++ DraggableWindows/Properties/AssemblyBuildReleaseVersionAttribute .cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+[AttributeUsage(AttributeTargets.Assembly)]
+public class AssemblyBuildReleaseVersionAttribute : Attribute
+{
+    public string Version { get; }
+
+    public AssemblyBuildReleaseVersionAttribute(string version)
+    {
+        Version = version;
+    }
+}
